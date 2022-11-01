02102bd2ab00284b3800168848bf952f7b120c0b
Ralf Krauth <website@halbwilde.com>
2022-11-01 20:46:26 +0100

    create jwt using rsa

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -2,13 +2,21 @@ import { Injectable } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
 import { compare } from 'bcrypt';
+import { readFileSync } from 'fs';
 
 @Injectable()
 export class AuthService {
+  private readonly privateKey: Buffer;
   constructor(
     private readonly usersService: UsersService,
     private readonly jwtService: JwtService,
-  ) {}
+  ) {
+    const privateKeyPath = process.env.TOKENKEY;
+    this.privateKey = readFileSync(privateKeyPath);
+  }
+
+
+
 
   async validateUser(username: string, pass: string): Promise<any> {
     const user = await this.usersService.findByUsername(username);
@@ -23,7 +31,7 @@ export class AuthService {
   async login(user: any) {
     const payload = { username: user.username, sub: user.userId };
     return {
-      access_token: this.jwtService.sign(payload),
+      access_token: this.jwtService.sign(payload, { privateKey: this.privateKey }),
     };
   }
 }
